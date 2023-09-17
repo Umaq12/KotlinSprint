83baa0f44fce72903db198701193adc81409ca7c
Umaq12 <feratj01@gmail.com>
2023-09-17 15:41:33 +0300

    KS-5-1 create simple authentication

--- src/main/kotlin/lesson5/task1.kt
+++ src/main/kotlin/lesson5/task1.kt
@@ -1,2 +1,13 @@
 package lesson5
 
+fun main () {
+    println("Докажите, что вы настоящий пользователь, для этого решите пример")
+    println("1 + 1")
+    println("Введите ответ:")
+    var answerOfCustomer = readln().toInt()
+    if (answerOfCustomer == 2) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
+    }
+}
